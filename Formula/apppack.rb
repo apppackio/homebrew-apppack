0897012bb3e0770ee15451e6b860ccb2c73e8da0
goreleaserbot <goreleaser@carlosbecker.com>
2021-01-27 11:03:35 -0700

    Brew formula update for apppack version v0.8.2

--- Formula/apppack.rb
+++ Formula/apppack.rb
@@ -5,21 +5,21 @@
 class Apppack < Formula
   desc "CLI for AppPack.io"
   homepage "https://apppack.io"
-  version "0.8.1"
+  version "0.8.2"
   license "Apache-2.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/apppackio/apppack/releases/download/v0.8.1/apppack_0.8.1_Darwin_x86_64.tar.gz"
-    sha256 "b806d7378c03918e0a8bb671961671ba6ad6bee15d4101cf72ad55dab8c619f8"
+    url "https://github.com/apppackio/apppack/releases/download/v0.8.2/apppack_0.8.2_Darwin_x86_64.tar.gz"
+    sha256 "6174ed8bfe7e53e2ae4be9005f229b84934bc62907c9afc144a54f89dfc9cd93"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/apppackio/apppack/releases/download/v0.8.1/apppack_0.8.1_Linux_x86_64.tar.gz"
-    sha256 "6e3fad964a3bcec0bf396609109132f9e4951a2ab3a525363077076f53b49a87"
+    url "https://github.com/apppackio/apppack/releases/download/v0.8.2/apppack_0.8.2_Linux_x86_64.tar.gz"
+    sha256 "4730c07c8e81aacc21fe28be0ff5dc60d6d6f76597ff9dbc7d05bc3b30500c2f"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/apppackio/apppack/releases/download/v0.8.1/apppack_0.8.1_Linux_arm64.tar.gz"
-    sha256 "0d92ce871e0d22ccc870966d45dec9f17efd21eb47083f27bf530044b4c9b841"
+    url "https://github.com/apppackio/apppack/releases/download/v0.8.2/apppack_0.8.2_Linux_arm64.tar.gz"
+    sha256 "92513c8a26a6f236a8e610a73c5b9d9fd604d444087b2247fbc0e1937b196362"
   end
 
   def install
