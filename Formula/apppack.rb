7535c82a89850f6ebaba958def1b081d5040937d
goreleaserbot <goreleaser@carlosbecker.com>
2021-03-08 12:36:52 -0700

    Brew formula update for apppack version v1.2.0

--- Formula/apppack.rb
+++ Formula/apppack.rb
@@ -11,15 +11,15 @@ class Apppack < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/apppackio/apppack/releases/download/v1.2.0/apppack_1.2.0_Darwin_x86_64.tar.gz"
-    sha256 "9afd284605e27080a3869a98382951a1e9b76f7c652acbc6a426da378432cb7a"
+    sha256 "ef61cae6626246113d9b0c8cc7678040292838985d484fc1d489eae090fff18a"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/apppackio/apppack/releases/download/v1.2.0/apppack_1.2.0_Linux_x86_64.tar.gz"
-    sha256 "cf263c41f2e151498cde6049a1b02af3af9dd98aa1e1201a97f4abcf4d10c79a"
+    sha256 "e239d59135fc9db12084ac14c5f9de7e3ec3d3b31d1a5b1ff2fa51a57303737a"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/apppackio/apppack/releases/download/v1.2.0/apppack_1.2.0_Linux_arm64.tar.gz"
-    sha256 "fbe5e3cd557acf7c871b05f32a9672ec9f418e65985b409160ae96bc71159109"
+    sha256 "751d53b10bd0c50cead8c833a70b233b2e1a7bed599a08de92823915da3aa0f0"
   end
 
   def install
