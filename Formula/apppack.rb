2da03cc3fc5880762d938281872c76431d8df687
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-29 11:12:10 -0600

    Brew formula update for apppack version v3.0.2

--- Formula/apppack.rb
+++ Formula/apppack.rb
@@ -5,21 +5,21 @@
 class Apppack < Formula
   desc "CLI for AppPack.io"
   homepage "https://apppack.io"
-  version "3.0.1"
+  version "3.0.2"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/apppackio/apppack/releases/download/v3.0.1/apppack_3.0.1_Darwin_arm64.tar.gz"
-      sha256 "34499898590d4b32b1b302cda9c49895f7b98cb989158c9bd9371302c29a3c3b"
+      url "https://github.com/apppackio/apppack/releases/download/v3.0.2/apppack_3.0.2_Darwin_arm64.tar.gz"
+      sha256 "36070b509bcb50c04e14710763db9f8668dd82bf8c0466bea3edc08b9040b71a"
 
       def install
         bin.install "apppack"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/apppackio/apppack/releases/download/v3.0.1/apppack_3.0.1_Darwin_x86_64.tar.gz"
-      sha256 "cf2976684b42cf140c6e00671df16de871ec4c2484b9babce738048e38e36f69"
+      url "https://github.com/apppackio/apppack/releases/download/v3.0.2/apppack_3.0.2_Darwin_x86_64.tar.gz"
+      sha256 "fafdff0de3825fd43a211d1929510905c2e2e4472939a5c26bcd8664459d5db4"
 
       def install
         bin.install "apppack"
@@ -28,17 +28,17 @@ class Apppack < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/apppackio/apppack/releases/download/v3.0.1/apppack_3.0.1_Linux_x86_64.tar.gz"
-      sha256 "6d067915d22f404a4c5f9f1b016d92353557e75be985d826a3dd2ac9449d3ac1"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/apppackio/apppack/releases/download/v3.0.2/apppack_3.0.2_Linux_arm64.tar.gz"
+      sha256 "bd64fb96daea4c1a01869426cfb7042a4173ea1c51f9665c0879ae51aaf1815e"
 
       def install
         bin.install "apppack"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/apppackio/apppack/releases/download/v3.0.1/apppack_3.0.1_Linux_arm64.tar.gz"
-      sha256 "bea7e3831eb4bb3981910fab186448d9bc259aa759b4ce4a569dfc89e6e9e7e9"
+    if Hardware::CPU.intel?
+      url "https://github.com/apppackio/apppack/releases/download/v3.0.2/apppack_3.0.2_Linux_x86_64.tar.gz"
+      sha256 "e524b001554d5b738f7762e7f7fc36de0ca17b8456760ef7cfa25e05c21d1399"
 
       def install
         bin.install "apppack"
